b19fe038d0f6d66dfd268e4138aa4090eea9c49d
iFallenHunt <lukas_santos.silva@hotmail.com>
2023-08-28 12:18:34 -0300

    Creation of a CRUD via REST api, running from docker with PostgreSQL database

---
+++ src/main/java/com/example/live/user/User.java
@@ -0,0 +1,45 @@
+package com.example.live.user;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "email")
+    private String email;
+
+    //getters and setter
+
+    public Long getId(){
+        return id;
+    }
+
+    public void setId(Long id){
+        this.id = id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+}
---
+++ src/main/java/com/example/live/user/UserController.java
@@ -0,0 +1,58 @@
+package com.example.live.user;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping
+    public List <User> getALLUser(){
+        return userRepository.findAll();
+    }
+
+    @GetMapping("/{id}") 
+    public User gettUserById(@PathVariable Long id){
+        return userRepository.findById(id).get();
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userRepository.save(user);
+    }
+
+    @PutMapping("/{id}")
+    public User updateUser(@PathVariable Long id, @RequestBody User user) {
+        User existingUser = userRepository.findById(id).get();
+        existingUser.setName(user.getName());
+        existingUser.setEmail(user.getEmail());
+        return userRepository.save(existingUser);
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteUser(@PathVariable Long id){
+        try{
+            //find the user
+            userRepository.findById(id).get();
+            userRepository.deleteById(id);
+            return "User deleted sucessfully";
+        } catch (Exception e ) {
+            //TODO: handle exception
+            return "User not found";
+        }
+    }    
+
+}
